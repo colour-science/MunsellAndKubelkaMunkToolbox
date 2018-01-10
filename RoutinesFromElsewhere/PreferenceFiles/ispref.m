37c0183c029c8ad041f48b07e6bd0ad7d525b401
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-01-10 22:54:25 +1300

    Change comments type in "RoutinesFromElsewhere/PreferenceFiles/ispref.m" file.

--- RoutinesFromElsewhere/PreferenceFiles/ispref.m
+++ RoutinesFromElsewhere/PreferenceFiles/ispref.m
@@ -1,37 +1,37 @@
-## Copyright (C) 2012 John W. Eaton
-##
-## This file is part of Octave.
-##
-## Octave is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or (at
-## your option) any later version.
-##
-## Octave is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with Octave; see the file COPYING.  If not, see
-## <http://www.gnu.org/licenses/>.
+% Copyright (C) 2012 John W. Eaton
+%
+% This file is part of Octave.
+%
+% Octave is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or (at
+% your option) any later version.
+%
+% Octave is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+% General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Octave; see the file COPYING.  If not, see
+% <http://www.gnu.org/licenses/>.
 
-## -*- texinfo -*-
-## @deftypefn {Function File} {} ispref (@var{group}, @var{pref})
-## Return true if the named preference @var{pref} exists in the
-## preference group @var{group}.
-##
-## The named preference group must be a character string.
-##
-## The preference @var{pref} may be a character string or a cell array
-## of character strings.
-##
-## If @var{pref} is not specified, return true if the preference
-## group @var{group} exists.
-## @seealso{getpref, addpref, setpref, rmpref}
-## @end deftypefn
+% -*- texinfo -*-
+% @deftypefn {Function File} {} ispref (@var{group}, @var{pref})
+% Return true if the named preference @var{pref} exists in the
+% preference group @var{group}.
+%
+% The named preference group must be a character string.
+%
+% The preference @var{pref} may be a character string or a cell array
+% of character strings.
+%
+% If @var{pref} is not specified, return true if the preference
+% group @var{group} exists.
+% @seealso{getpref, addpref, setpref, rmpref}
+% @end deftypefn
 
-## Author: jwe
+% Author: jwe
 
 function retval = ispref (group, pref)
 
